f5d73a566ce7c7bad68f7411546a7cf2a40aad4c
Karolis Blazevicius <blaz@danskebank.lt>
2018-09-06 13:13:03 +0300

    Open api for internal network on 5001 port.

--- CarSharing/CarSharing/Program.cs
+++ CarSharing/CarSharing/Program.cs
@@ -19,6 +19,7 @@ namespace CarSharing
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5001")
                 .UseStartup<Startup>();
     }
 }
