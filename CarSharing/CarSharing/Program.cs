1b5441b58075d293a8598284bcd17c282312ce32
Karolis Blazevicius <blaz@danskebank.lt>
2018-09-06 19:09:32 +0300

    use port 8080

--- CarSharing/CarSharing/Program.cs
+++ CarSharing/CarSharing/Program.cs
@@ -12,7 +12,7 @@ namespace CarSharing
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://0.0.0.0:5001")
+                .UseUrls("http://0.0.0.0:8080")
                 .UseStartup<Startup>();
     }
 }
